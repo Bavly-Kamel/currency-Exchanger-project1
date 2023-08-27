a75522d5f7cab17376ef3d7cc546afbd98deafc3
Mohamed <mohamedmabrook190@gmail.com>
2023-08-27 12:47:21 +0300

    Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,13 +12,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiComponent } from './ui/ui.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UiConvertComponent } from './ui/ui-convert/ui-convert.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AddToFavComponent } from './add-to-fav/add-to-fav.component';
 import { LiveExchangeRatesComponent } from './live-exchange-rates/live-exchange-rates.component';
 import { PortfolioCardComponent } from './live-exchange-rates/portfolio-card/portfolio-card.component';
+import {UiCompareComponent} from "./ui/ui-compare/ui-compare.component";
 
 
 
@@ -31,7 +33,6 @@ import { PortfolioCardComponent } from './live-exchange-rates/portfolio-card/por
     AddToFavComponent,
     LiveExchangeRatesComponent,
     PortfolioCardComponent
-
   ],
   imports: [
     BrowserModule,
