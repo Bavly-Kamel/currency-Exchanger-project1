045d53a3dfc6d96ad07cd8f14edff45521027a9d
Mohamed <mohamedmabrook190@gmail.com>
2023-08-27 12:47:21 +0300

    Portfolio

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,22 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ICurrency } from '../models/currency.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
+  baseURL = 'http://www.amrcurrencyconversion.site';
 
-  constructor(public http : HttpClient) {
-    
-   }
+  constructor(private http: HttpClient) {}
+
+  getPortfolio() {
+    let myPortfolio = localStorage.getItem('myPortfolio')
+      ? JSON.parse(localStorage.getItem('myPortfolio') || '')
+      : [];
+    return myPortfolio;
+  }
+
+  updatePortfolio(currency: ICurrency) {
+    let myPortfolio = this.getPortfolio();
+    if (currency.selected) {
+      myPortfolio.push(currency);
+    } else {
+      myPortfolio = myPortfolio.filter(
+        (c: ICurrency) => c.code != currency.code
+      );
+    }
+    localStorage.setItem('myPortfolio', JSON.stringify(myPortfolio));
+  }
+
+  getCurrencies(): Observable<ICurrency[]> {
+    return this.http.get(`${this.baseURL}/api/v1`).pipe(
+      map((res: any) => {
+        if (res) {
+          return res.currencies as ICurrency[];
+        } else {
+          throw new Error('Failed to retrieve currencies!');
+        }
+      })
+    );
+  }
 
    convertCurrency(amount : number ,  From : string , TO : string ){
      return this.http.get(  ` http://13.51.146.2/api/v1/conversion?from=${From}&to=${TO}&amount=${amount}` )
    }
   compareCurrency(amount : number , From : string , TO : string []){
     return this.http.get(   `http://13.51.146.2/api/v1/comparison?from=${From}&amount=${amount}&list=${TO}`)
-   }
-   getCurrencies(){
-    return this.http.get( `http://13.51.146.2/api/v1`)
    }
 }
