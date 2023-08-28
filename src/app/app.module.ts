f1152cfa4167685b699d271ad943a64381850ebd
Rwazeer <rwazeer@banquemisr.com>
2023-08-28 21:08:16 +0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
