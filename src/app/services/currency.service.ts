b0e9ba01e9112350fcb109e9786b8258d2723e0f
nouranhakiem <nouranahmed708@gmail.com>
2023-08-29 09:45:48 +0300

    ip change

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -11,10 +11,10 @@ export class CurrencyService {
    }
 
    convertCurrency(amount : number ,  From : string , TO : string ){
-     return this.http.get(  ` http://13.51.146.2/api/v1/conversion?from=${From}&to=${TO}&amount=${amount}` )
+     return this.http.get(  ` http://51.20.2.168/api/v1/conversion?from=${From}&to=${TO}&amount=${amount}` )
    }
   compareCurrency(amount : number , From : string , TO : string []){
-    return this.http.get(   `http://13.51.146.2/api/v1/comparison?from=${From}&amount=${amount}&list=${TO}`)
+    return this.http.get(   `http://51.20.2.168/api/v1/comparison?from=${From}&amount=${amount}&list=${TO}`)
    }
    getCurrencies(){
     return this.http.get( `http://13.51.146.2/api/v1`)
