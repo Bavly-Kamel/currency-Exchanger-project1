176ece3bdd7454ec3e57f8faa78149bf9feca199
Bavly <137098046+Bavly-Kamel@users.noreply.github.com>
2023-08-29 13:20:05 +0300

    api-change

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -2,19 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrencyService {
+  constructor(public http: HttpClient) {}
 
-  constructor(public http : HttpClient) {
-    
-   }
-
-   convertCurrency(amount : number ,  From : string , TO : string ){
-     return this.http.get(  ` http://51.20.4.55/api/v1/conversion?from=${From}&to=${TO}&amount=${amount}` )
-   }
-  compareCurrency(amount : number , From : string , TO : string []){
-    return this.http.get(   `http://51.20.4.55/api/v1/comparison?from=${From}&amount=${amount}&list=${TO}`)
-   }
-   
+  convertCurrency(amount: number, From: string, TO: string) {
+    return this.http.get(
+      ` http://51.20.4.55/api/v1/conversion?from=${From}&to=${TO}&amount=${amount}`
+    );
+  }
+  compareCurrency(amount: number, From: string, TO: string[]) {
+    return this.http.get(
+      `http://51.20.4.55/api/v1/comparison?from=${From}&amount=${amount}&list=${TO}`
+    );
+  }
+  getCurrencies() {
+    return this.http.get(`http://51.20.4.55/api/v1`);
+  }
 }
